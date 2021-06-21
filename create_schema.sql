8fd459479b8a70b514104f2a1ee9487f24907197
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-21 15:40:51 -0500

    Jonas' SQL Basics challenge

--- create_schema.sql
+++ create_schema.sql
@@ -1,5 +1,5 @@
 -- Schema
-DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS students CASCADE;
 CREATE TABLE students (
   id           serial PRIMARY KEY,
   first_name   varchar(255) NOT NULL,
@@ -7,3 +7,37 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+DROP TABLE IF EXISTS addresses CASCADE;
+CREATE TABLE addresses (
+id            serial PRIMARY KEY, 
+line_1        varchar(255) NOT NULL,
+city          varchar(255) NOT NULL,
+state         varchar(255) NOT NULL,
+zipcode       INTEGER
+);
+
+DROP TABLE IF EXISTS classes CASCADE;
+CREATE TABLE classes (
+id            serial PRIMARY KEY,
+name          varchar(255) NOT NULL,
+credits       varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS enrollments CASCADE;
+CREATE TABLE enrollments (
+id            serial PRIMARY KEY,
+student_id    INTEGER,
+class_id      INTEGER,
+grade         varchar(255)
+);
+
+ALTER TABLE enrollments
+ADD CONSTRAINT student_id
+FOREIGN KEY (student_id) 
+REFERENCES students (id);
+
+ALTER TABLE enrollments
+ADD CONSTRAINT class_id
+FOREIGN KEY (class_id) 
+REFERENCES classes (id);
