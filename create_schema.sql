2b3f284d5f12186e0197e241b414fb796299f0e5
vzbrunstad <vzbrunstad@msn.com>
2021-06-23 14:43:43 -0500

    John and Vince SQL-Basics

--- create_schema.sql
+++ create_schema.sql
@@ -1,5 +1,17 @@
--- Schema
-DROP TABLE IF EXISTS students;
+-- addresses schema
+DROP TABLE IF EXISTS addresses; 
+
+CREATE TABLE addresses (
+  id         serial PRIMARY KEY,
+  line_1     varchar(255) NOT NULL,
+  city       varchar(255) NOT NULL,
+  state      varchar(30) NOT NULL,
+  zipcode    varchar(5) NOT NULL
+);
+
+-- Students Schema
+DROP TABLE IF EXISTS students; 
+
 CREATE TABLE students (
   id           serial PRIMARY KEY,
   first_name   varchar(255) NOT NULL,
@@ -7,3 +19,23 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+-- classes schema
+DROP TABLE IF EXISTS classes;
+
+CREATE TABLE classes (
+  id          serial PRIMARY KEY,
+  name        varchar(255) NOT NULL,
+  credits     integer
+);
+
+-- enrollments schema
+DROP TABLE IF EXISTS enrollments;
+
+CREATE TABLE enrollments (
+  id          serial PRIMARY KEY,
+  student_id  integer NOT NULL,
+  class_id    integer NOT NULL,
+  grade       varchar(2)
+);
+
